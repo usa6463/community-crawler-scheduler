c69b3fb947a3f77fd8178eb643f4c6d094e8703f
usa6463 <usa6463@gmail.com>
2022-04-22 23:40:43 +0900

    UPDATE dc-scheduler.py 참조 이미지 변경 및 환경변수 설정 테스트

--- dc-scheduler.py
+++ dc-scheduler.py
@@ -4,25 +4,29 @@ from airflow.decorators import dag
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
+from kubernetes.client import V1EnvVar
 
 default_args = {
     'owner': 'airflow',
 }
 
 
-@dag(default_args=default_args, schedule_interval="@daily", start_date=datetime(2022, 3, 12), max_active_runs=1)
+@dag(default_args=default_args, schedule_interval="@daily", start_date=datetime(2022, 4, 20), max_active_runs=1)
 def dc_scrapping():
     k = KubernetesPodOperator(
         name="scrap-dc-stock",  # pod name
         namespace="default",
-        image="usa6463/community-crawler:1.0.1",
-        arguments=["--target_date", "{{ next_ds }}",
-                   "--last_content_num", "2430001",
-                   "--elasticsearch_hostname", "elasticsearch-master.default.svc.cluster.local",
-                   "--elasticsearch_port", "9200",
-                   "--elasticsearch_index_name", "dc-content-test"],
+        env_vars=[
+            V1EnvVar(name="ES_HOST", value="elasticsearch-master.default.svc.cluster.local"),
+            V1EnvVar(name="TARGET_DATE", value="{{ ds }}")
+        ],
+        image="usa6463/community-crawler:2.0.0",
+        # arguments=["--target_date", "{{ next_ds }}",
+        #            "--last_content_num", "2430001",
+        #            "--elasticsearch_hostname", "elasticsearch-master.default.svc.cluster.local",
+        #            "--elasticsearch_port", "9200",
+        #            "--elasticsearch_index_name", "dc-content-test"],
         task_id="scrap-dc-stock",
-        do_xcom_push=True,
     )
 
     k.dry_run()
