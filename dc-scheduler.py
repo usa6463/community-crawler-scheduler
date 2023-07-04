715e20dba12b0a1fb16d45cdb3452d9ea04031bd
usa6463 <usa6463@gmail.com>
2023-07-04 19:43:26 +0900

    Update scrap-dc-man-fashion image version up 2.2.19 -> 2.2.20

--- dc-scheduler.py
+++ dc-scheduler.py
@@ -47,7 +47,7 @@ default_args = {
 }
 
 
-@dag(default_args=default_args, schedule_interval="@daily", start_date=datetime(2023, 7, 1), max_active_runs=1)
+@dag(default_args=default_args, schedule_interval="@daily", start_date=datetime(2023, 7, 2), max_active_runs=1)
 def dc_scrapping():
     man_fashion_gall = KubernetesPodOperator(
         name="scrap-dc-man-fashion",  # pod name
