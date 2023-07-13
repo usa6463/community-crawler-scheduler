5fab142f021151a3dc07ff85b51d6d48d50e4c17
usa6463 <usa6463@gmail.com>
2023-07-13 20:57:10 +0900

    Update image version 2.2.21

--- dc-scheduler.py
+++ dc-scheduler.py
@@ -47,7 +47,7 @@ default_args = {
 }
 
 
-@dag(default_args=default_args, schedule_interval="@daily", start_date=datetime(2023, 7, 2), max_active_runs=1)
+@dag(default_args=default_args, schedule_interval="@daily", start_date=datetime(2023, 7, 11), max_active_runs=1)
 def dc_scrapping():
     man_fashion_gall = KubernetesPodOperator(
         name="scrap-dc-man-fashion",  # pod name
@@ -62,7 +62,7 @@ def dc_scrapping():
             V1EnvVar(name="WEB_DRIVER_PATH", value="/chromedriver"),
             V1EnvVar(name="POLITENESS", value="1500"),
         ],
-        image="usa6463/community-crawler:2.2.20",
+        image="usa6463/community-crawler:2.2.21",
         task_id="scrap-dc-man-fashion",
         container_resources=k8s_models.V1ResourceRequirements(
             limits={"memory": "2G", "cpu": "2000m"},
